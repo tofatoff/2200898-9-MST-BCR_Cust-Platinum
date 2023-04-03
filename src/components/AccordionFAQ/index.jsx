795f153ecb0d0b3c722e895daa93f5d1e0d2e0fb
Tofa <mail.mstofa@gmail.com>
2023-04-03 10:30:01 +0700

    fix FAQ issues

--- src/components/AccordionFAQ/index.jsx
+++ src/components/AccordionFAQ/index.jsx
@@ -7,40 +7,23 @@ import {
 } from 'reactstrap';
 
 const AccordionFAQ = (props) => {
-    const [open, setOpen] = useState('');
-    const toggle = (id) => {
-        if (open === id) {
-            setOpen();
-        } else {
-            setOpen(id);
-        }
-    };
-
     return (
         <div>
-            <Accordion open={open} toggle={toggle}
-                style={{
-                    border: '1px solid #D4D4D4',
-                    borderRadius: '4px',
-                    width: '668px',
-                    height: '56px'
-                }}>
-                <AccordionItem>
-                    <AccordionHeader targetId="1"
-                        style={{
-                            fontFamily: 'Arial',
-                            fontStyle: 'normal',
-                            fontWeight: '700',
-                            fontSize: "14px",
-                            lineHeight: '20px'
-                        }}>
-                        {props.question}
-                    </AccordionHeader>
-                    <AccordionBody accordionId="1">
-                        {props.answer}
-                    </AccordionBody>
-                </AccordionItem>
-            </Accordion>
+            <AccordionItem>
+                <AccordionHeader targetId={props.id}
+                    style={{
+                        fontFamily: 'Arial',
+                        fontStyle: 'normal',
+                        fontWeight: '700',
+                        fontSize: "14px",
+                        lineHeight: '20px'
+                    }}>
+                    {props.question}
+                </AccordionHeader>
+                <AccordionBody accordionId={props.id}>
+                    {props.answer}
+                </AccordionBody>
+            </AccordionItem>
         </div >
     );
 }
--- src/pages/Home/components/FAQ/index.jsx
+++ src/pages/Home/components/FAQ/index.jsx
@@ -1,25 +1,48 @@
 import React, { useState } from 'react';
-import { Container } from 'reactstrap';
+import {
+    Container,
+    Accordion,
+    AccordionBody,
+    AccordionHeader,
+    AccordionItem,
+} from 'reactstrap';
 import AccordionFAQ from '../../../../components/AccordionFAQ';
 
 const FAQ = () => {
+    const [open, setOpen] = useState('');
+    const toggle = (id) => {
+        if (open === id) {
+            setOpen('');
+        } else {
+            setOpen(id);
+        }
+    };
 
     return (
-        <Container className='d-flex justify-content-between'>
-            <section id="faq" >
+        <section id="faq" >
+            <Container className='d-flex justify-content-between flex-wrap'>
                 <div>
                     <h2>Frequently Asked Question</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
                 </div>
-                <div>
-                    <AccordionFAQ question="Apa saja syarat yang dibutuhkan?" answer='Sample Answer Text' /><br />
-                    <AccordionFAQ question="Berapa hari minimal sewa mobil lepas kunci?" answer='Sample Answer Text' /><br />
-                    <AccordionFAQ question="Berapa hari sebelumnya sabaiknya booking sewa mobil?" answer='Sample Answer Text' /><br />
-                    <AccordionFAQ question="Apakah Ada biaya antar-jemput?" answer='Sample Answer Text' /><br />
-                    <AccordionFAQ question="Bagaimana jika terjadi kecelakaan" answer='Sample Answer Text' /><br />
+                <div
+                    style={{
+                        // width: '60%',
+                        // marginLeft: '100px'
+                    }}
+                >
+                    <Accordion open={open} toggle={toggle}
+                    >
+                        <AccordionFAQ question="Apa saja syarat yang dibutuhkan?" answer='Sample Answer Text' id='1' /><br />
+                        <AccordionFAQ question="Berapa hari minimal sewa mobil lepas kunci?" answer='Sample Answer Text' id='2' /><br />
+                        <AccordionFAQ question="Berapa hari sebelumnya sabaiknya booking sewa mobil?" answer='Sample Answer Text' id='3' /><br />
+                        <AccordionFAQ question="Apakah Ada biaya antar-jemput?" answer='Sample Answer Text' id='4' /><br />
+                        <AccordionFAQ question="Bagaimana jika terjadi kecelakaan" answer='Sample Answer Text' id='5' /><br />
+
+                    </Accordion>
                 </div>
-            </section>
-        </Container>
+            </Container>
+        </section>
     )
 }
 
