edadaad5027f8028149c5a1632b18eb3a2d00dfd
Uda Awe <awe.alingga23@gmail.com>
2023-05-18 21:32:48 +0700

    add button lanjutkan pembayaran

--- src/pages/Detail/components/DetailMobil/index.jsx
+++ src/pages/Detail/components/DetailMobil/index.jsx
@@ -1,79 +1,99 @@
-import React from 'react'
-import { FiUsers } from 'react-icons/fi'
+import React from "react";
+import { FiUsers } from "react-icons/fi";
+import { Button } from "reactstrap";
 
 const CATEGORY_OPTION = {
-    small: '2 - 4 orang',
-    medium: '4 - 6 orang',
-    large: '6 - 8 orang',
+  small: "2 - 4 orang",
+  medium: "4 - 6 orang",
+  large: "6 - 8 orang",
 };
 
 const DetailMobil = (props) => {
-    const strToCurrIDR = (strnum) => {
-        const formatterIDR = new Intl.NumberFormat("id-ID", {
-            style: "currency",
-            currency: "IDR",
-            minimumFractionDigits: 0,
-            maximumFractionDigits: 0,
-        });
-        return formatterIDR.format(parseInt(strnum));//ga pake parseInt jg bisa
-    }
-    return (
-        <div
-            style={{
-                width: '405px',
-                height: '435px',
-                background: '#FFFFFF',
-                boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.15)',
-                borderRadius: '8px',
-                padding: '24px'
-            }}
+  const strToCurrIDR = (strnum) => {
+    const formatterIDR = new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+    return formatterIDR.format(parseInt(strnum)); //ga pake parseInt jg bisa
+  };
+  return (
+    <div
+      style={{
+        width: "405px",
+        height: "435px",
+        background: "#FFFFFF",
+        boxShadow: "0px 0px 4px rgba(0, 0, 0, 0.15)",
+        borderRadius: "8px",
+        padding: "24px",
+      }}
+    >
+      <img src={props.image} alt="" width="100%" />
+      <p
+        style={{
+          fontFamily: "Arial",
+          fontStyle: "normal",
+          fontWeight: "700",
+          fontSize: "14px",
+          lineHeight: "20px",
+        }}
+      >
+        {props.name}
+      </p>
+      <div>
+        <FiUsers />
+        <span
+          style={{
+            fontFamily: "Arial",
+            fontStyle: "normal",
+            fontSize: "14px",
+            lineHeight: "20px",
+          }}
         >
-            <img src={props.image} alt="" width="100%" />
-            <p
-                style={{
-                    fontFamily: 'Arial',
-                    fontStyle: 'normal',
-                    fontWeight: '700',
-                    fontSize: '14px',
-                    lineHeight: '20px',
-                }}
-            >{props.name}</p>
-            <div>
+          {CATEGORY_OPTION[props.category]}
+        </span>
+      </div>
+      <p>Tentukan lama sewa mobil (max. 7 hari)</p>
+      <div className="d-flex justify-content-between align-items-center">
+        <span
+          style={{
+            fontFamily: "Arial",
+            fontStyle: "normal",
+            fontWeight: "700",
+            fontSize: "14px",
+            lineHeight: "20px",
+          }}
+        >
+          Total
+        </span>
+        <span
+          style={{
+            fontFamily: "Arial",
+            fontStyle: "normal",
+            fontWeight: "700",
+            fontSize: "14px",
+            lineHeight: "20px",
+          }}
+        >
+          {strToCurrIDR(props.price)}
+        </span>
+      </div>
+      <div>
+        <Button
+          style={{
+            backgroundColor: "#5CB85F",
+            fontSize: "14px",
+            fontFamily: "Arial",
+            fontWeight: "700",
+          }}
+          block
+        >
+          Lanjutkan Pembayaran
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-                <FiUsers />
-                <span
-                    style={{
-                        fontFamily: 'Arial',
-                        fontStyle: 'normal',
-                        fontSize: '14px',
-                        lineHeight: '20px',
-                    }}
-                >{CATEGORY_OPTION[props.category]}</span>
-            </div>
-            <div className='d-flex justify-content-between align-items-center'>
-                <span
-                    style={{
-                        fontFamily: 'Arial',
-                        fontStyle: 'normal',
-                        fontWeight: '700',
-                        fontSize: '14px',
-                        lineHeight: '20px',
-                    }}
-                >Total</span>
-                <span
-                    style={{
-                        fontFamily: 'Arial',
-                        fontStyle: 'normal',
-                        fontWeight: '700',
-                        fontSize: '14px',
-                        lineHeight: '20px',
-                    }}
-                >{strToCurrIDR(props.price)}</span>
-
-            </div>
-
-        </div >
-    )
-}
-
-export default DetailMobil
+export default DetailMobil;
--- src/pages/Home/components/Testimonial/index.jsx
+++ src/pages/Home/components/Testimonial/index.jsx
@@ -1,23 +1,22 @@
-import { Container } from "reactstrap"
-import CarouselExample from "../../../../components/CarouselExample"
+import { Container } from "reactstrap";
+// import CarouselExample from "../../../../components/CarouselExample"
 
-import CarouselTestimonial from "../../../../components/CarouselTestimonial"
+import CarouselTestimonial from "../../../../components/CarouselTestimonial";
 
 const Testimonial = () => {
+  return (
+    <section id="testimonial">
+      <Container>
+        <div>
+          <h2>Testimonial</h2>
+          <p>Berbagai review positif dari para pelanggan kami</p>
+        </div>
 
-    return (
-        <section id="testimonial">
-            <Container>
-                <div>
-                    <h2>Testimonial</h2>
-                    <p>Berbagai review positif dari para pelanggan kami</p>
-                </div>
+        <CarouselTestimonial />
+        {/* <CarouselExample /> */}
+      </Container>
+    </section>
+  );
+};
 
-                <CarouselTestimonial />
-                {/* <CarouselExample /> */}
-            </Container>
-        </section>
-    )
-}
-
-export default Testimonial
+export default Testimonial;
