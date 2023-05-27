46c0b43f2f2cb27a59e52594a0be5285b685491d
ibnasitinh <ibna.siti@gmail.com>
2023-05-27 20:33:28 +0700

    add login & register

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,10 +1,57 @@
-import { Col, Form, Row } from "reactstrap"
+import { Col, Form, Row, Toast } from "reactstrap"
 import imgLogin from "../../assets/img_login.png"
 import LogoBCR from "../../components/LogoBCR"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { setUserSession } from "../../utils/common"
 
 const Register = () => {
+
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    async function registerUser(params) {
+        return fetch('https://bootcamp-rent-cars.herokuapp.com/customer/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setLoading(true)
+
+        let params = {
+            email: email,
+            password: password,
+            name: name
+        }
+        
+        const response = await registerUser(params);
+        
+        const isSuccessful = response.ok;
+        const data = await response.json()
+            .then((result) => {
+                if (!isSuccessful) {
+                    setError(result.message);
+                } else {
+                    alert("Register telah berhasil");
+                    //ini kondisi login success,
+                    // setUserSession(result);
+
+                    //redirect ke halaman mana jika login sukses
+
+                }
+                setLoading(false)
+            });
+
+    };
 
     return (
         <>
@@ -35,7 +82,7 @@ const Register = () => {
                                 <input type="password" id="form2Example2" className="form-control" name="password" onChange={e => setPassword(e.target.value)} />
                             </div>
 
-                            <button className="btn btn-primary btn-block mb-4" type="submit" disabled={loading} >{loading ? 'Loading...' : 'Login'} </button>
+                            <button className="btn btn-primary btn-block mb-4" type="submit" disabled={loading} >{loading ? 'Loading...' : 'Sign Up'} </button>
 
                             <div className="text-center">
                                 <p>Already have an account<a href="#!">Sign In here</a></p>
